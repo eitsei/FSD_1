6fcb8d5c7bcc49b9644462124d6c7fb18118cdaf
eitsei <aj.asunmaa@gmail.com>
2024-06-14 18:30:25 +0300

    cs

--- OSA4/tests/blog.test.js
+++ OSA4/tests/blog.test.js
@@ -6,7 +6,8 @@ const app = require('../app')
 const api = supertest(app)
 const helper = require('./test_helper')
 const Blog = require('../models/blog')
-
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
 
 
 describe('when there is initially some blogs saved', () => {
@@ -131,25 +132,25 @@ describe('when there is initially some blogs saved', () => {
       assert(!blogs.includes(`${blogToDelete.title}`))
     })
     test('Adding one like to a blog, amount should be +1', async () => {
-        const blogs = await helper.blogsInDb()
-        const blogToModifyi = blogs[0]
-  
-        const modifyidBlog = {
-          title: blogToModifyi.title,
-          author: blogToModifyi.author,
-          url:    blogToModifyi.url,
-          likes:  blogToModifyi.likes + 1
-        }
-  
-        await api
-          .put(`/api/blogs/${blogToModifyi.id}`)
-          .send(modifyidBlog)
-          .expect(200)
-  
-        const blogsAtEnd = await helper.blogsInDb()
-        assert.notDeepStrictEqual(blogToModifyi.likes,blogsAtEnd[0].likes)
-        assert.deepStrictEqual(blogsAtEnd[0].likes, blogToModifyi.likes + 1)
-      })
+      const blogs = await helper.blogsInDb()
+      const blogToModifyi = blogs[0]
+
+      const modifyidBlog = {
+        title: blogToModifyi.title,
+        author: blogToModifyi.author,
+        url:    blogToModifyi.url,
+        likes:  blogToModifyi.likes + 1
+      }
+
+      await api
+        .put(`/api/blogs/${blogToModifyi.id}`)
+        .send(modifyidBlog)
+        .expect(200)
+
+      const blogsAtEnd = await helper.blogsInDb()
+      assert.notDeepStrictEqual(blogToModifyi.likes,blogsAtEnd[0].likes)
+      assert.deepStrictEqual(blogsAtEnd[0].likes, blogToModifyi.likes + 1)
+    })
 
     test('Likes of modified blog should be 100 and it should not match original likes', async () => {
       const blogs = await helper.blogsInDb()
@@ -173,6 +174,62 @@ describe('when there is initially some blogs saved', () => {
     })
   })
 })
-after(async () => {
-    await mongoose.connection.close()
+
+describe('when there is initially one user at db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
   })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    assert(usernames.includes(newUser.username))
+  })
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+
+    assert(result.body.error.includes('expected `username` to be unique'))
+
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+  })
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
